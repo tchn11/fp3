a5cb751701378602043b6d3a91cca8386f601575
Tonya Konanykhina <81990607+tchn11@users.noreply.github.com>
2024-03-13 01:33:32 +0300

    delete setq from main

--- src/main.lisp
+++ src/main.lisp
@@ -2,28 +2,36 @@
 (load "src/generator.lisp")
 (load "src/inputs.lisp")
 
-(defun main (points generator line)
-  (let ((currentx (next generator))
-        (input-points (if line (list (read-from-files points) (read-from-files points))
-							(list (read-from-files points) (read-from-files points) (read-from-files points) (read-from-files points))))
-        (out-list (list nil)))
-    (loop while currentx do
+(defun main-loop (points generator line out-list input-points)
+	(let ((currentx (next generator)))
+    (if currentx
+		(let ((points-iter
 		(let ((second-point (car (last input-points))))
-		      (when (> currentx (get-x second-point))
+		      (if (> currentx (get-x second-point))
 			  	(let ((new-point (read-from-files points)))
-				(when new-point
-				(setq input-points (cdr input-points))
-				(push new-point (cdr (last input-points)))))))
+				(if new-point
+					(progn
+					(push new-point (cdr (last input-points)))
+					(cdr input-points))
+					input-points))
+				input-points))))
 	  (print currentx)
 	  (write-string " ")
 	  (let ((result
 		 (if line
-		     (line (coerce input-points 'vector) currentx)
-		   (degree input-points currentx))))
+		     (line (coerce points-iter 'vector) currentx)
+		   (degree points-iter currentx))))
 	    (write result)
 	    (push (list currentx result) (cdr (last out-list))))
-	  (setq currentx (next generator)))
-    out-list))
+		(main-loop points generator line out-list points-iter))
+	out-list)))
+
+(defun main (points generator line)
+(let ((input-points (if line (list (read-from-files points) (read-from-files points))
+							(list (read-from-files points) (read-from-files points) (read-from-files points) (read-from-files points))))
+        (out-list (list nil)))
+		(main-loop points generator line out-list input-points)
+))
 
 (defun run-main ()
 	(main (open-file (ask "Enter filename:")) (range 8 1 0) (string= (ask "Line method? (y/n)") "y")))
