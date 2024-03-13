a7c90c2f568c361b2f82cd8434dae49c0353c61b
thcn11 <314929@niuitmo.ru>
2024-03-13 09:26:36 +0300

    Fix format

--- src/main.lisp
+++ src/main.lisp
@@ -3,18 +3,18 @@
 (load "src/inputs.lisp")
 
 (defun main-loop (points generator line out-list input-points)
-	(let ((currentx (next generator)))
+  (let ((currentx (next generator)))
     (if currentx
-		(let ((points-iter
-		(let ((second-point (car (last input-points))))
-		      (if (> currentx (get-x second-point))
-			  	(let ((new-point (read-from-files points)))
-				(if new-point
-					(progn
-					(push new-point (cdr (last input-points)))
-					(cdr input-points))
-					input-points))
-				input-points))))
+	(let ((points-iter
+	       (let ((second-point (car (last input-points))))
+		 (if (> currentx (get-x second-point))
+		     (let ((new-point (read-from-files points)))
+		       (if new-point
+			   (progn
+			     (push new-point (cdr (last input-points)))
+			     (cdr input-points))
+			 input-points))
+		   input-points))))
 	  (print currentx)
 	  (write-string " ")
 	  (let ((result
@@ -23,15 +23,15 @@
 		   (degree points-iter currentx))))
 	    (write result)
 	    (push (list currentx result) (cdr (last out-list))))
-		(main-loop points generator line out-list points-iter))
-	out-list)))
+	  (main-loop points generator line out-list points-iter))
+      out-list)))
 
 (defun main (points generator line)
-(let ((input-points (if line (list (read-from-files points) (read-from-files points))
-							(list (read-from-files points) (read-from-files points) (read-from-files points) (read-from-files points))))
+  (let ((input-points (if line (list (read-from-files points) (read-from-files points))
+			(list (read-from-files points) (read-from-files points) (read-from-files points) (read-from-files points))))
         (out-list (list nil)))
-		(main-loop points generator line out-list input-points)
-))
+    (main-loop points generator line out-list input-points)
+    ))
 
 (defun run-main ()
   (main (open-file (ask "Enter filename:")) (range 8 1 0) (string= (ask "Line method? (y/n)") "y")))
